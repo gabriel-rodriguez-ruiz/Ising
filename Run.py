2334b53b8ab1af5199379638e3b413143b013f10
0xInfty <anirix.vall@gmail.com>
2018-12-15 15:18:52 -0300

    Scripts 'ising2D1' + 'En.py' + 'ising2Dpaso.py'.
    - Cambio de nombre 'ising2Dpaso.py' a 'Run.py' (me gusta usar notación en minúscula y con guiones bajos para los módulos como 'ising' y notación de mayúsculas sin espacios ni guiones
    bajos para los
    scripts).
    - Lo adapto para que use las funciones desde el módulo.
    - Traduzco comentarios al inglés.
    - Cambio algunos nombres de variables para que sean más explícitos (en Spyder existe un truco mágico que es apretar tab después de escribir los primeros caracteres del nombre de una
    variable; bendito sea el autocompletar que convierte 'mag'+Tab en 'magnetization').
    - Separo el código en secciones con '#%%' (en mi Spyder puedo usar Ctrl+Shift+Enter para correr una sección y pasar a la siguiente ó Ctrl+Enter para correr una sección sin mover el
    cursor).
    - Agrego un par de plots intermedios y quito la opción de 'figure' que los sobreescribe.
    - Separo los indicadores de tiempo por sección.
    
    Problemas que me gustaría solucionar:
    - ¡Es muy lento!
    - Estaría copado que corra al menos una animación mientras lo hace para no aburrirse (?)
    
    ¡Atención!
    - Borro archivos 'En.py', 'ising2Dpaso.py' porque ahora su contenido está en 'ising.py' (para ver esos archivos se puede usar 'git checkout cc7e656')

---
+++ Run.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 10 14:24:05 2018
+
+@author: Gabriel, Valeria.
+"""
+
+# General script for a run
+
+import numpy as np
+#import matplotlib.axes
+import matplotlib.pyplot as plt
+import ising as ing
+import time
+
+#%% User's parameters
+
+Lx = 15 # x size
+Ly = 15 # y size
+#beta_critico = log(1+sqrt(2))/2 = 0.44069
+beta = 0.5 # 1/kT
+
+npre = 2000 # amount of steps used to pre-thermalize
+nsteps = 5000 # amount of steps used post-thermalization
+nplot = 100 # amount of steps between plots
+
+#%% Initial state
+
+# S is a random matrix whose elements are 1 or -1 (spin projections)
+S = ing.initial_condition('hot', (Lx, Ly))
+energy = np.zeros(nsteps + 1)
+magnetization = np.zeros(nsteps + 1)
+
+fig = plt.figure()
+fig.add_axes()
+ax = fig.gca()
+ax.pcolormesh(S.T)
+plt.title("Estado inicial")
+plt.xlabel("X (u.a.)")
+plt.ylabel("Y (u.a.)")
+
+#%% Pre-thermalization
+
+start = time.time()
+
+for n in range(npre):
+    S = ing.ising_step(S, beta)[0]
+    print("Step: {:.0f}/{:.0f}".format(n+1, npre))
+
+energy[0] = ing.energy(S)
+magnetization[0] = np.sum(S)
+
+stop = time.time()
+enlapsed = stop - start
+print("Enlapsed time: {:.2f} s".format(enlapsed))
+print("Current energy: {:.2f}".format(energy[0]))
+print("Current magnetization: {:.2f}".format(magnetization[0]))
+
+fig = plt.figure()
+fig.add_axes()
+ax = fig.gca()
+ax.pcolormesh(S.T)
+plt.title("Estado pretermalizado")
+plt.xlabel("X (u.a.)")
+plt.ylabel("Y (u.a.)")
+
+#%% Post-thermalization
+
+start = time.time()
+
+for n in range(nsteps):
+    S, dE, dM = ing.ising_step(S, beta)
+    energy[n + 1] += energy[n] + dE
+    magnetization[n + 1] = magnetization[n] + dM
+    print("Step: {:.0f}/{:.0f}".format(n+1, nsteps))
+
+stop = time.time()
+enlapsed = stop - start
+print("Enlapsed time: {:.2f} s".format(enlapsed))
+print("Current energy: {:.2f}".format(energy[-1]))
+print("Current magnetization: {:.2f}".format(magnetization[-1]))
+
+fig = plt.figure()
+fig.add_axes()
+ax = fig.gca()
+ax.pcolormesh(S.T)
+plt.title("Estado final")
+plt.xlabel("X (u.a.)")
+plt.ylabel("Y (u.a.)")
+
+#%% Some general plots
+
+"""    
+    if n%nshow==0:
+        ax.pcolormesh(S)
+        plt.title("T = {}".format(1/beta)) #llos graficos no funcionan
+"""
+
+plt.figure()
+plt.subplot(2,1,1)
+plt.plot(energy, "o-")
+plt.ylabel("Energía")
+
+plt.subplot(2,1,2)
+plt.plot(magnetization, "o-")
+plt.ylabel("Magnetización")
+plt.xlabel("Paso")
